aa6f443136b4666fc356824b723f0108357d8a74
Ingvild Tisland <ingvildtisland@Ingvilds-Air-5.uia.no>
2019-03-26 14:31:46 +0100

    Oppgave5c

--- ICA3/Oppgave5/HTTP_client/http_client.go
+++ ICA3/Oppgave5/HTTP_client/http_client.go
@@ -1,4 +1,5 @@
 package main
+
 //Kode laget utifra eksempel på: https://dlintw.github.io/gobyexample/public/http-client.html
 
 import (
@@ -8,6 +9,11 @@ import (
 	"os"
 )
 
+/** OPPGAVE 5C. "func main" sender forespørsel til serveren :8989 om å få koble seg til.
+http.Get metoden returnerer 2 verdier. 1: respons objektet som i dette tilfellet er "ok"
+og 2: Hvis det skjer en feil i tilkoblingen til serveren og å få en respons tilbake, altså
+hvis err ikke er lik nil vil det komme en feilmelding  */
+
 func main() {
 	resp, err := http.Get("http://localhost:8989/ok")
 	if err != nil {
@@ -16,11 +22,12 @@ func main() {
 
 	defer resp.Body.Close()
 
+	//Når klienten har fått kontakt med serveren leser den "bodyen fra serveren"
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
-
+	//Skriver "bodyen" fra serveren som i dette tilfellet er nameEmail strukturen hentet fra json.go
 	_, err = os.Stdout.Write(body)
 	if err != nil {
 		log.Fatal(err)
--- ICA3/Oppgave5/HTTP_server/http_server.go
+++ ICA3/Oppgave5/HTTP_server/http_server.go
@@ -6,12 +6,16 @@ import (
 
 	"github.com/IngvildT/IS105-Gruppe13-mappe/ICA-1-3-Repository/ICA3/Oppgave5/json"
 )
+/** OPPGAVE 5C 
+func main starter serveren og søker etter klienter. */
 
 func main() {
 	http.HandleFunc("/ok", handler)
 	http.ListenAndServe(":8989", nil)
 }
 
+/** OPPGAVE 5C. "func handler" funksjonen henter JSON strukturen fra json.go pakken
+og skriver til klienter ved forespørsel. */
 func handler(w http.ResponseWriter, r *http.Request) {
 	s := json.JSON()
 	fmt.Fprintf(w, string(s))
