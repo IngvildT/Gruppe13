17e6ed9099c8acf9cb78d19d5eeea1e5e2053ed0
IngvildT <ingvit18@uia.no>
2019-04-15 19:11:06 +0200

    Oppgave 3

--- ICA4/Oppgave3/google/googlespeech.go
+++ ICA4/Oppgave3/google/googlespeech.go
@@ -12,12 +12,8 @@ import (
 	speechpb "google.golang.org/genproto/googleapis/cloud/speech/v1"
 )
 
-<<<<<<< HEAD
 //export GOOGLE_APPLICATION_CREDENTIALS="/Users/ingvildtisland/go/src/github.com/IngvildT/IS105-Gruppe13-mappe/ICA-1-3-Repository/ICA4/Oppgave3/google/newjson.json"
 
-=======
-// export GOOGLE_APPLICATION_CREDENTIALS="/Users/juliekjellevold/Downloads/api.json"
->>>>>>> f65384e406888ca4d165ccb446a05e36904f7079
 func main() {
 	ctx := context.Background()
 
@@ -28,11 +24,7 @@ func main() {
 	}
 
 	// Sets the name of the audio file to transcribe.
-<<<<<<< HEAD
 	filename := "./test.wav"
-=======
-	filename := "./audio.wav"
->>>>>>> f65384e406888ca4d165ccb446a05e36904f7079
 
 	// Reads the audio file into memory.
 	data, err := ioutil.ReadFile(filename)
@@ -44,11 +36,7 @@ func main() {
 	resp, err := client.Recognize(ctx, &speechpb.RecognizeRequest{
 		Config: &speechpb.RecognitionConfig{
 			Encoding:        speechpb.RecognitionConfig_LINEAR16,
-<<<<<<< HEAD
 			SampleRateHertz: 22050,
-=======
-			SampleRateHertz: 16000,
->>>>>>> f65384e406888ca4d165ccb446a05e36904f7079
 			LanguageCode:    "en-US",
 		},
 		Audio: &speechpb.RecognitionAudio{
--- ICA4/Oppgave3/wit/wit_main.go
+++ ICA4/Oppgave3/wit/wit_main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"fmt"
 
-	speech "github.com/IngvildT/wit/go-speak"
+	speech "github.com/IngvildT/IS105-Gruppe13-mappe/ICA-1-3-Repository/ICA4/Oppgave3/wit/go-speak"
 )
 
 //example
 
 func main() {
 	speech.SetWitKey("NQKGDPHCW465PKZIAWQ3D4RM5C5KCGYE") //Wit API Key MUST be set before calling any other Wit.AI functions
-	a := speech.SendWitVoice("./test.wav")
+	a := speech.SendWitVoice("./audio.wav")
 	fmt.Println(a)
 }
