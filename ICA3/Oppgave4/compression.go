18f4d3163aab6bbf4a3f21c62d544a252cf25677
IngvildT <ingvit18@uia.no>
2019-04-16 19:13:57 +0200

    Kode fix

--- ICA3/Oppgave4/compression.go
+++ ICA3/Oppgave4/compression.go
@@ -21,8 +21,10 @@ func main() {
 	} //Hvis åpningen av filen failer printes en errormelding.
 
 	fil := string(file)
+	filTotalt := fmt.Sprintf("%d", len(fil))
+	fmt.Println("Antall bytes i fil opprinnelig: " + filTotalt + "\n")
 
-	// Henter Hex funksjonen fra pipe, teller antall bytes totalt og printer de 20 første bytesene.
+	// Henter Hex funksjonen fra pipe, teller antall bytes totalt og printer de 20 første.
 	hexString := pipe.Hex(fil)
 	hexTotalt := fmt.Sprintf("%d", len(hexString))
 	hexSlice := hexString[:20]
@@ -30,7 +32,7 @@ func main() {
 	fmt.Println("Antall bytes totalt: " + hexTotalt + "\n" +
 		"Innhold 20 første bytes: " + hexSlice + "\n")
 
-	// Henter base64 funksjonen fra pipe, teller antall bytes totalt og printer de 20 første bytesene.
+	// Henter base64 funksjonen fra pipe, teller antall bytes totalt og printer de 20 første.
 	base64String := pipe.Base64(fil)
 	base64Totalt := fmt.Sprintf("%d", len(base64String))
 	base64Slice := base64String[:20]
@@ -38,7 +40,7 @@ func main() {
 	fmt.Println("Antall bytes totalt: " + base64Totalt + "\n" +
 		"Innhold 20 første bytes: " + base64Slice + "\n")
 
-	// Henter Gzip funksjonen fra pipe, teller antall bytes totalt og printer de 20 første bytesene.
+	// Henter Gzip funksjonen fra pipe, teller antall bytes totalt og printer de 20 første.
 	gzipString := pipe.Gzip(fil)
 	gzipTotalt := fmt.Sprintf("%d", len(gzipString))
 	gzipSlice := gzipString[:20]
