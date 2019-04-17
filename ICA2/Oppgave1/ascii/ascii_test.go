5a822cd4f6bcb31fbedc588d118186d14b59d6c8
IngvildT <ingvit18@uia.no>
2019-04-17 13:58:32 +0200

    Endring

---
+++ ICA2/Oppgave1/ascii/ascii_test.go
@@ -0,0 +1,24 @@
+package ascii
+
+import "testing"
+import "fmt"
+
+/** Oppgave 1b
+Test for funksjonen ​greetingASCII() ​i egen fil ascii_test.go​,
+som tester om returverdier (av type ​string​) inneholderkun ASCII-tegn. */
+
+func TestGreetingASCII(t *testing.T) {
+	if !(rangeASCII(GreetingASCII())) {
+		t.Fail()
+	}
+}
+
+func rangeASCII(s string) bool {
+	for _, c := range s {
+		fmt.Println(c)
+		if c == 128 {
+			return false
+		}
+	}
+	return true
+}
