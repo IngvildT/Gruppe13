acdb8eadaaacc35ddf492b237ca37bc1ab1941ee
IngvildT <ingvit18@uia.no>
2019-04-17 13:50:30 +0200

    Endring

--- ICA1-01/Oppgave2/java/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld {
-
-    public static void main(String[] args) {
-        // Prints "Hello, 世界" to the terminal window.
-        System.out.println("Hello, 世界");
-    }
-
-}
