73fb0dfffc47f318e88992c9781d46cf1f9b8dcb
IngvildT <ingvit18@uia.no>
2019-04-18 10:50:59 +0200

    Removed api

---
+++ ICA4/Oppgave3/wit/wit_main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	speech "github.com/IngvildT/IS105-Gruppe13-mappe/ICA-1-3-Repository/ICA4/Oppgave3/wit/go-speak"
+)
+
+//example
+
+func main() {
+	speech.SetWitKey("personal key") //Wit API Key MUST be set before calling any other Wit.AI functions
+	a := speech.SendWitVoice("./audio.wav")
+	fmt.Println(a)
+}
