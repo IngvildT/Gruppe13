89b3426aff4c04f9ddf21943251ede687773fad5
IngvildT <ingvit18@uia.no>
2019-04-22 13:10:27 +0200

    Oppgave3c

---
+++ ICA2/Oppgave3/treasure_main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	treasure "github.com/IngvildT/IS105-Gruppe13-mappe/ICA-REPO/ICA2/Oppgave3/treasure"
+)
+
+func main() {
+
+	treasure.PrintTreasureUTF8(treasure.TreasureString)
+}
