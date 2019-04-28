fb921adacefabd7aa799c8c852a56a4e0aea8c75
IngvildT <ingvit18@uia.no>
2019-04-28 22:16:31 +0200

    yess

--- ICA4/Oppgave3/wit/wit_main.go
+++ ICA4/Oppgave3/wit/wit_main.go
@@ -1,15 +1,38 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 
-	speech "github.com/IngvildT/IS105-Gruppe13-mappe/ICA-1-3-Repository/ICA4/Oppgave3/wit/go-speak"
+	speech "github.com/IngvildT/IS105-Gruppe13-mappe/ICA-REPO/ICA4/Oppgave3/wit/go-speak"
 )
 
-//example
+type SpeechToText struct {
+	Text     string `json:"_text"`
+	Outcomes []struct {
+		Confidence interface{} `json:"confidence"`
+		Intent     string      `json:"intent"`
+		Text       string      `json:"_text"`
+		Entities   struct {
+		} `json:"entities"`
+	} `json:"outcomes"`
+	WARNING string `json:"WARNING"`
+	MsgID   string `json:"msg_id"`
+}
 
 func main() {
-	speech.SetWitKey("personal key") //Wit API Key MUST be set before calling any other Wit.AI functions
-	a := speech.SendWitVoice("./audio.wav")
-	fmt.Println(a)
-}
+
+	speech.SetWitKey("NQKGDPHCW465PKZIAWQ3D4RM5C5KCGYE") //Wit API Key MUST be set before calling any other Wit.AI functions
+	a := []byte(speech.SendWitVoice("./audio.wav"))
+	//fmt.Println(a)
+
+	var text SpeechToText
+	err := json.Unmarshal(a, &text)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	fmt.Printf("%+v", text.Text)
+}	
+
+	
+	
