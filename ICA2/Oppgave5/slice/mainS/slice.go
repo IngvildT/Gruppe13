a5be1c701ba00a67aeaf7a4ae5400a34612debc9
julikj18 <julikj18@uia.no>
2019-04-30 12:49:25 +0200

    Endring oppgave5 slice

---
+++ ICA2/Oppgave5/slice/mainS/slice.go
@@ -0,0 +1,29 @@
+package slice
+
+func AllocateMake(b int) []byte {
+	//
+	s := make([]byte, b)
+	// returnerer en slice av typen byte ved bruk av make metoden
+	return s
+}
+func AllocateVar(b int) []byte {
+
+	// variabel s (slice) bruker både var og make til å lage en slice med argument b
+	var s []byte = make([]byte, b)
+
+	// Returnerer en slice av typen byte
+	return s
+}
+
+// Reslice funksjonen skal slice en allerede eksisterende slice
+// ved bruk av en av Allocatefunksjonene
+func Reslice(a []byte, lidx int, uidx int) []byte {
+
+	//lager en ny slice av allocateMake til å allokere plass i minnet til en slice
+	slice1 := AllocateMake(20)
+
+	slice1 = a[lidx:uidx]
+
+	return slice1
+
+}
